79aaf185e0f44a7956b5cda8097c6d27297a69b2
Víctor San Martín <vsanmartinm@gmail.com>
2016-04-25 01:47:56 -0300

    test with service workers

---
+++ js/sw.js
@@ -0,0 +1,38 @@
+var cacheName = 'feriados-1';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/js/index.js',
+  '/css/index.css',
+  '/img/giftly.png'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache).then(function() {
+        return self.skipWaiting();
+      });
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
